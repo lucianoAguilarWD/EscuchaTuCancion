9a4444139f5410c5c1b918e29afd83e0c155c1b7
lucianoAguilarWD <166865911+lucianoAguilarWD@users.noreply.github.com>
2024-06-24 12:35:34 -0300

    ajuste dos

--- app/src/main/java/com/example/escuchatucancion/SwipeAlbumActivity.java
+++ app/src/main/java/com/example/escuchatucancion/SwipeAlbumActivity.java
@@ -3,6 +3,7 @@ package com.example.escuchatucancion;
 import android.content.Intent;
 import android.os.Bundle;
 
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
